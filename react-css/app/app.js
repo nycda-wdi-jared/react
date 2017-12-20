53e251512827f10ca7a3890254e8fe8968db289f
Jared Thomas <jjthom87@yahoo.com>
2017-12-20 15:10:00 -0500

    readme

--- react-css/app/app.js
+++ react-css/app/app.js
@@ -1,16 +1,9 @@
-// FRONT-END: React Router
-// Rendering our Router Component with {routes} component; Passing down {browser history}
-
-// DEPENDENCIES================================================================
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-// Importing all of the routes contained in the {routes} component
 import routes from './routes.js';
 
-// ENTRY POINT (to application)==================================================
-// Rendering application and mounting to DOM
 ReactDOM.render(
-    <Router history={browserHistory}>{routes}</Router>,
-  document.getElementById('app')
+	<Router history={browserHistory}>{routes}</Router>,
+	document.getElementById('app')
 );
--- react-css/app/routes.js
+++ react-css/app/routes.js
@@ -1,18 +1,11 @@
-// FRONT-END: React Routes
-// Establishing our Component hierarchy for passing down states/props
-// Setting route paths for link navigation
-
-// DEPENDENCIES================================================================
 import React from "react";
-import { Route, IndexRoute, browserHistory } from "react-router";
+import { Route } from "react-router";
 
-// REQUIRE COMPONENTS==========================================================
 // Parent
 import Application from "./components/Application";
 // Children
 import Home from "./components/Home";
 
-// COMPONENT ROUTES============================================================
 export default (
   	<Route component={Application}>
     	<Route path="/" component={Home} />
