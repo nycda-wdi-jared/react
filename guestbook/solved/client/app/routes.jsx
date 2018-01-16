4a5161477589f8d522dd75608e9b5e7ff7e6f543
Jared Thomas <jjthom87@yahoo.com>
2018-01-15 22:35:06 -0500

    fixes

--- guestbook/solved/client/app/routes.jsx
+++ guestbook/solved/client/app/routes.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { Route } from "react-router";
 
-// Parent
-import Application from "./components/Application";
-// Children
 import Guestbook from "./components/Guestbook";
 
 export default (
-  	<Route component={Application}>
-    	<Route path="/" component={Guestbook} />
-    </Route>
+    <Route path="/" component={Guestbook} />
 );
